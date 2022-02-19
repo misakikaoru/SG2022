43775901fc4f6595ee29a5a7b26bbaf0b827c638
MisakiKaoru@cct <kaoru.misaki@cct-inc.co.jp>
2022-02-19 11:27:59 +0900

    tweet

--- SG2022/SG2022/ViewModels/AboutViewModel.cs
+++ SG2022/SG2022/ViewModels/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CoreTweet;
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -11,8 +14,24 @@ namespace SG2022.ViewModels
 		{
 			Title = "About";
 			OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+			TweetCommand = new Command(OnTweetClicked);
 		}
 
 		public ICommand OpenWebCommand { get; }
+		public Command TweetCommand { get; }
+
+		private void OnTweetClicked(object obj)
+		{
+		    string ConsumerKey = "";
+		    string ConsumerSecret = "";
+		    string AccessToken = "";
+		    string AccessSecret = "";
+
+            var session = OAuth.Authorize(ConsumerKey, ConsumerSecret);
+            var client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            Tokens tokens = Tokens.Create(ConsumerKey, ConsumerSecret, AccessToken, AccessSecret);
+            Status statusResult = tokens.Statuses.Update(new { status = "test tweet"});
+		}
 	}
 }
