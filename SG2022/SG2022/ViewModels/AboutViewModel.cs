b5f637de42ac0fab7e13e61586851c137fac1b15
MisakiKaoru@cct <kaoru.misaki@cct-inc.co.jp>
2022-02-20 21:28:37 +0900

    tweet by pin

--- SG2022/SG2022/ViewModels/AboutViewModel.cs
+++ SG2022/SG2022/ViewModels/AboutViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CoreTweet;
 using System;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -14,24 +16,37 @@ namespace SG2022.ViewModels
 		{
 			Title = "About";
 			OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
-			TweetCommand = new Command(OnTweetClicked);
+			TweetPinCommand = new Command(OnTweetPinClicked);
+			Url = session.AuthorizeUri.AbsoluteUri;
 		}
 
 		public ICommand OpenWebCommand { get; }
 		public Command TweetCommand { get; }
+		public Command TweetPinCommand { get; }
+		private WebViewSource url;
+		public WebViewSource Url { get => url; set => SetProperty(ref url, value); }
+		private string pinCode;
+		public string PinCode { get => pinCode; set => SetProperty(ref pinCode, value); }
 
-		private void OnTweetClicked(object obj)
+		//API key
+		static string ConsumerKey = "";
+		//API secret key
+		static string ConsumerSecret = "";
+		OAuth.OAuthSession session = OAuth.Authorize(ConsumerKey, ConsumerSecret);
+
+		private void OnTweetPinClicked(object obj)
 		{
-		    string ConsumerKey = "";
-		    string ConsumerSecret = "";
-		    string AccessToken = "";
-		    string AccessSecret = "";
-
-            var session = OAuth.Authorize(ConsumerKey, ConsumerSecret);
-            var client = new HttpClient();
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-            Tokens tokens = Tokens.Create(ConsumerKey, ConsumerSecret, AccessToken, AccessSecret);
-            Status statusResult = tokens.Statuses.Update(new { status = "test tweet"});
+			if (string.IsNullOrEmpty(PinCode)) return;
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+			try
+			{
+				// トークン取得
+				var browserTokens = OAuth.GetTokens(session, PinCode);
+				Status statusResult = browserTokens.Statuses.Update(new { status = "test tweet by user" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss ffff") });
+			}
+			catch (Exception exception)
+			{
+			}
 		}
 	}
 }
