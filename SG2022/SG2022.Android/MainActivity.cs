66a100ba36ef946abbcdd96b09f430f220811926
MisakiKaoru@cct <kaoru.misaki@cct-inc.co.jp>
2022-02-21 21:49:18 +0900

    two icons

--- SG2022/SG2022.Android/MainActivity.cs
+++ SG2022/SG2022.Android/MainActivity.cs
@@ -7,11 +7,17 @@ using Android.OS;
 
 namespace SG2022.Droid
 {
-    [Activity(Label = "SG2022", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SG2022", Icon = "@mipmap/icon", 
+        //Theme = "@style/MainTheme",
+        Theme = "@style/SplashTheme", 
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Window.RequestFeature(Android.Views.WindowFeatures.ActionBar);
+            SetTheme(Resource.Style.MainTheme);
+            
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
---
+++ SG2022/SG2022.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace SG2022.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { this.SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        private async void SimulateStartup()
+        {
+            await Task.Delay(100);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
